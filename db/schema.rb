cb545c0281725ccd8a36a0020a48bf788b63f198
gokdenizozkan <gokdeniz@gokdenizozkan.com>
2024-05-30 14:44:14 +0300

    add migration to remove status & add published

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_22_081307) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_113648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,9 +55,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_22_081307) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "body"
-    t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "published", default: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
