33e31a8b8cbeaa1b50f66f448895ab40b30059c5
gokdenizozkan <gokdeniz@gokdenizozkan.com>
2024-05-30 16:05:56 +0300

    add update & destroy conditions => reject if published

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,11 +3,29 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 140 }
   has_rich_text :body
+
   before_save :default_values
+  before_update :prevent_update_if_published
+  before_destroy :prevent_destroy_if_published
 
   def default_values
     unless self.body.present?
       self.body = "content will be here"
+    end
+  end
+
+  def prevent_update_if_published
+    if :published
+      puts "in if published"
+      errors.add(:base, 'Cannot update if it is published')
+      throw(:abort)
+    end
+  end
+
+  def prevent_destroy_if_published
+    if :published
+      errors.add :published, :not_allowed, message: "Cannot destroy if it is published"
+      throw :abort
     end
   end
 end
