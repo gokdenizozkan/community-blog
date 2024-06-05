f4a28a6ad5bd6238bcee8a9a18a3c5536e9cc561
gokdenizozkan <gokdeniz@gokdenizozkan.com>
2024-06-05 16:02:39 +0300

    add capability to update comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
 	before_action :authenticate_user!
 	before_action :set_comment, only: [:edit, :update, :destroy]
+	before_action :set_article, only: [:update]
 
 	def new
 	  @comment = Comment.new params[:article_id]
@@ -19,8 +20,15 @@ class CommentsController < ApplicationController
 	end
 
 	def update
-		@comment.update comment_params
-		redirect_back fallback_location: root_path
+		@comment = @article.comments.find params[:id]
+
+		respond_to do |format|
+			if @comment.update comment_params
+				format.html { redirect_to article_url @article, notice: 'Comment has been updated.' }
+			else
+				format.html { redirect_to article_url @article, alert: 'Comment was not updated.' }
+			end
+		end
 	end
 
 	def destroy
@@ -54,6 +62,10 @@ class CommentsController < ApplicationController
 	end
 
 	private
+	def set_article
+		@article = Article.find params[:article_id]
+	end
+
 	def set_comment
 	  @comment = Comment.find(params[:id])
 	end
---
+++ app/javascript/controllers/comments_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {}
+  toggleForm(event) {
+  	event.preventDefault();
+  	event.stopPropagation();
+  	
+  	const formId = event.params["form"];
+  	const commentBodyId = event.params["body"];
+  	
+  	const form = document.getElementById(formId);
+  	form.classList.toggle("none");
+
+  	const commentBody = document.getElementById(commentBodyId);
+  	commentBody.classList.toggle("none");
+  }
+}
