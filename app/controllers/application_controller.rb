0438fc5cceed3a0d2dd9d7cc0bb44c6b3c04ab32
gokdenizozkan <gokdeniz@gokdenizozkan.com>
2024-06-12 13:14:20 +0300

    add Turkish localization files for views and models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
   def authorize_against_current_user(user_id)
     unless current_user.id == user_id.to_i
       redirect_back fallback_location: root_path
     end
+  end
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
   end
 end
